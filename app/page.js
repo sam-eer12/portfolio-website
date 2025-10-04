2e4045bc8e504419f2859627732bffc8c48ee613
Sameer Gupta <sameer.gupta.ug24@nsut.ac.in>
2025-10-04 12:43:36 +0530

    ll

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@
 
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import About from "./components/About";
 export default function Home() {
   return (
     <>
     <Navbar />
     <Header />
+    <About />
     </>
   );
 }
